3504ae534841c816f38394e51f68acdc49ae36c4
erincodes <erinleeannscott@gmail.com>
2018-02-05 21:15:13 -0700

    Added JS to change the text color of paragraph-one id

---
+++ script.js
@@ -0,0 +1,6 @@
+/*
+* Tier I of assignment challenge options = change text color.
+*
+* Reference https://bootcamp-coders.cnm.edu/class-materials/javascript/events/assignment.php
+ */
+document.getElementById("paragraph-one").style.color = "magenta";
